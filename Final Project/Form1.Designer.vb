24128f26d546ef404aaad851b7856c5f2113add4
Evan <Evan@EVAN-PC>
2016-05-06 20:08:24 -0500

    Add project files.

---
+++ Final Project/Form1.Designer.vb
@@ -0,0 +1,98 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(101, 54)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Sim Season"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(230, 54)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(366, 54)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Button3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(627, 54)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(240, 150)
+        Me.DataGridView1.TabIndex = 4
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(366, 258)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(120, 95)
+        Me.ListBox1.TabIndex = 5
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1086, 580)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents ListBox1 As ListBox
+End Class
---
+++ Final Project/Form1.vb
@@ -0,0 +1,88 @@
+﻿Imports Final_Project
+
+Public Class Form1
+    Public day As Integer = 0
+    Dim baseball As New baseball
+
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If day = 0 Then day += 1
+        If day = 1 Then
+            baseball.GameSim(baseball.Cardinals, baseball.Pirates)
+        End If
+        ListBox1.Items.Add(baseball.Cardinals.wins)
+    End Sub
+
+
+
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+    End Sub
+End Class
+Public Class team
+    Public wins As Integer
+    Public losses As Integer
+    Public elo As Integer = elobase
+    Public Const elobase As Integer = 1000
+End Class
+
+Public Class baseball
+    Public Cubs As New team
+    Public Cardinals As New team
+    Public Pirates As New team
+    Public Red As New team
+    Public Brewers As New team
+    Public Phillies As New team
+    Public Marlins As New team
+    Public Mets As New team
+    Public Nationals As New team
+    Public Braves As New team
+    Public Giants As New team
+    Public Padres As New team
+    Public Dodgers As New team
+    Public Rockies As New team
+    Public Diamondbacks As New team
+    Public Yankees As New team
+    Public RedSox As New team
+    Public Rays As New team
+    Public Orioles As New team
+    Public BlueJays As New team
+    Public WhiteSox As New team
+    Public Indians As New team
+    Public Royals As New team
+    Public Twins As New team
+    Public Tigers As New team
+    Public Astros As New team
+    Public Angels As New team
+    Public Athletics As New team
+    Public Mariners As New team
+    Public Rangers As New team
+    Public Sub GameSim(ByRef x As team, ByRef y As team)
+        Dim score1 As Integer
+        Dim score2 As Integer
+        Dim inning As Integer = 9
+        Randomize()
+        score1 = CInt(Math.Floor((20 - 1) * Rnd()))
+        Randomize()
+        score2 = CInt(Math.Floor((20 - 1) * Rnd()))
+        Do Until score1 <> score2
+            If score1 > score2 Then
+                x.wins += 1
+                y.losses += 1
+            End If
+            If score1 < score2 Then
+                y.wins += 1
+                x.losses += 1
+            End If
+            If score1 = score2 Then
+                Randomize()
+                score1 += CInt(Math.Floor((1 - 0 + 1) * Rnd())) + 0
+                Randomize()
+                score2 += CInt(Math.Floor((1 - 0 + 1) * Rnd())) + 0
+            End If
+            inning += 1
+        Loop
+    End Sub
+End Class
